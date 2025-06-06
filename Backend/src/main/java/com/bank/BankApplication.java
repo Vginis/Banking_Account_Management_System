ca976cfd551eb64c571b5590d58b1f2e0f56a0a8
Vaggelis Gkinis <evangelos.gkinis@eurodyn.com>
2025-06-06 12:27:27 +0300

    Add component scan for Service layer

--- Backend/src/main/java/com/bank/BankApplication.java
+++ Backend/src/main/java/com/bank/BankApplication.java
@@ -3,10 +3,12 @@ package com.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.bank.repository")
 @EntityScan("com.bank.domain")
+@ComponentScan("com.bank")
 @SpringBootApplication
 public class BankApplication {
 
